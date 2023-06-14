6068b053753d1b9ecc10f3cacc6e9ed7903f12ec
Kumar Kalpadiptya Roy <kalpadiptya.roy@outlook.com>
2023-06-14 14:38:15 +0530

    Added PORT using .env file and setup cors and body parsing.

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,9 @@ const dotenv = require("dotenv");
 const app = express();
 app.use(express.json());
 
+dotenv.config({path: '../.env'}); // We mention the path to the .env file.
+const port = process.env.PORT_DEV || 5000;
+
+// Setup cors policy for http request
+app.use(cors());
+app.use(express.json());
